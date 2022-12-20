59bd8590d6d79a663180bb6e4b075342ff543524
wu.zeMin <244@qq.com>
2022-12-20 13:46:30 +0800

    chore: 添加插件注释

--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,7 @@ import { viteMockServe as Mock } from "vite-plugin-mock";
 import Inspect from "vite-plugin-inspect";
 import { compression } from "vite-plugin-compression2";
 
+// https://cn.vitejs.dev/config/
 export default defineConfig(() => ({
   resolve: {
     alias: {
@@ -15,17 +16,34 @@ export default defineConfig(() => ({
     },
   },
   plugins: [
+    // https://github.com/vitejs/vite-plugin-vue2/#readme
     Vue(),
+
+    // https://github.com/windicss/vite-plugin-windicss
     WindiCSS(),
+
+    // https://github.com/vitejs/vite/tree/main/packages/plugin-legacy#readme
     Legacy(),
+
+    // https://github.com/hannoeru/vite-plugin-pages
     Pages(),
+
+    // https://github.com/antfu/unplugin-vue-components
     Components(),
+
+    // https://github.com/vbenjs/vite-plugin-mock/blob/main/README.zh_CN.md
     Mock({
       ignore: /^_/,
       injectCode: `import '../mock/_createProductionServer';`,
     }),
+
+    // https://github.com/antfu/vite-plugin-inspect#readme
     Inspect(),
+
+    // https://cn.vitejs.dev/guide/build.html#chunking-strategy
     splitVendorChunkPlugin(),
+
+    // https://github.com/nonzzz/vite-compression-plugin#readme
     compression({ threshold: 1024 * 50 }),
   ],
 }));
--- windi.config.js
+++ windi.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from "windicss/helpers";
 
+// https://windicss.org/
 export default defineConfig({
   shortcuts: {
     "flex-center": "flex justify-center items-center",
